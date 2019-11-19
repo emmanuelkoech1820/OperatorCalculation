40d24ffed7b4bd2e73845a43c165bb179b2423da
emmanuelkoech1820 <emmanuelkoech1820@gmail.com>
2019-11-19 18:09:22 +0300

    Add project files.

---
+++ OperatorCalculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OperatorCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the First number: ");
+            double number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Operator: ");
+            String Operator = Console.ReadLine();
+
+            Console.Write("Enter the Second number: ");
+            double number2 = Convert.ToDouble(Console.ReadLine());
+
+
+            if (Operator == "+")
+            {
+                Console.WriteLine("The answer is: {0}", number1 + number2);
+            }
+            else if (Operator == "-")
+            {
+                Console.WriteLine("The answer is: {0}", number1 - number2);
+            }
+            else if (Operator == "/")
+            {
+                Console.WriteLine("The answer is: {0}", number1 / number2);
+            }
+            else if (Operator == "*")
+            {
+                Console.WriteLine("The answer is: {0}", number1 * number2);
+            }
+            else   //gtghytgcvtcrredef
+            {
+                Console.WriteLine("Invalid Operator");
+            }
+            Console.ReadLine();
+        }
+    }
+}
